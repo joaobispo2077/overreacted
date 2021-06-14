004839349e44890cce7aafc5c1e840815e763615
João Bispo <joaobispo2077@gmail.com>
2021-06-14 15:01:07 -0300

    test(button): should be disabled when disabled is false

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -30,4 +30,11 @@ describe('<Button />', () => {
 
     expect(button).toBeDisabled();
   });
+  it('should be disabled when disabled is false', () => {
+    render(<Button text="Load more" disabled={false} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+
+    expect(button).toBeEnabled();
+  });
 });
